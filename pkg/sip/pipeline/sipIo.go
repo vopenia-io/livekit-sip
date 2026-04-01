6bcc8f94b31daaf2346a89856dcdc11f21890a58
RedCommand <senard.maxime@gmail.com>
2026-04-01 14:24:19 +0200

    fix: comment out PCMA caps for g711-audio until resolved

--- pkg/sip/pipeline/sipIo.go
+++ pkg/sip/pipeline/sipIo.go
@@ -42,7 +42,7 @@ func (sio *SipIo) Create() error {
 
 	formatCaps := []*gst.Caps{
 		gst.NewCapsFromString("application/x-rtp,media=audio,encoding-name=PCMU,clock-rate=8000"),
-		gst.NewCapsFromString("application/x-rtp,media=audio,encoding-name=PCMA,clock-rate=8000"),
+		// gst.NewCapsFromString("application/x-rtp,media=audio,encoding-name=PCMA,clock-rate=8000"), // TODO: fix g711-audio and then enable that back
 		gst.NewCapsFromString("application/x-rtp,media=video,encoding-name=H264,clock-rate=90000"),
 	}
 
