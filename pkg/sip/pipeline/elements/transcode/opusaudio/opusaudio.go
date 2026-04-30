d903b12ba8e2eb25af7e60b75629966f1a20bb6b
RedCommand <senard.maxime@gmail.com>
2026-04-30 18:38:51 +0200

    chore: handle opus-audio add error

--- pkg/sip/pipeline/elements/transcode/opusaudio/opusaudio.go
+++ pkg/sip/pipeline/elements/transcode/opusaudio/opusaudio.go
@@ -88,13 +88,17 @@ func (e *OpusAudio) InstanceInit(instance *glib.Object) {
 		return
 	}
 
-	self.AddMany(
+	if err := self.AddMany(
 		e.RtpOpusDepay,
 		e.OpusDec,
 		e.AudioConvert,
 		e.AudioResample,
 		e.AudioRate,
-	)
+	); err != nil {
+		self.Log(CAT, gst.LevelError, fmt.Sprintf("Failed to add elements to bin: %v", err))
+		self.Error("Failed to add elements to bin", err)
+		return
+	}
 
 	if err := gst.ElementLinkMany(
 		e.RtpOpusDepay,
